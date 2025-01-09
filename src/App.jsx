2319aa0cdc05355bf6d3eb8953e1693a72c75346
Vishnurj11 <vishnuvishnurajc11@gmail.com>
2025-01-09 15:24:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Product from './components/Product'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Product>
+        </Product>
+        </div>
+    </>
+  )
+}
+
+export default App
